93ca08b83c77aaa38ff5c640a69370d2c709cad2
Gemmechu Mohammed Hassena <gemmechumohammed@gmail.com>
2021-07-17 17:50:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,19 +6,19 @@ DEBUG = False
 THREADS_PER_PAGE = 2
 
 APP_NAME = 'Demo Face Embedding'
-path = 'best_state.pth'
-# models = [
-#     dict(
-#         model=model_920,
-#         acc=0.920
-#     ),
-#     dict(
-#         model=model_921,
-#         acc=.92135
-#     )
-# ]
+# path = 'best_state.pth'
+models = [
+    dict(
+        model=model_920,
+        acc=0.920
+    ),
+    dict(
+        model=model_921,
+        acc=.92135
+    )
+]
 
-# USE_MODEL = models[1]
-# SECRET_KEY = "cBWegL8d367vPzTp9Y2pJudLLtaKi5Jtw8//WsRjZrc="
+USE_MODEL = models[1]
+SECRET_KEY = "cBWegL8d367vPzTp9Y2pJudLLtaKi5Jtw8//WsRjZrc="
 USE_MODEL = FaceNetModel
 
